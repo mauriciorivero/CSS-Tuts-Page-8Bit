653562207fc776ecb849bcb0f216ca57f5abc13f
mauriciorivero <rivero.mauricio@gmail.com>
2021-10-26 21:43:03 -0500

    Octubre_26_2021_2

---
+++ js/power-up.js
@@ -0,0 +1,5 @@
+var count_tutorial1 = 0;
+document.getElementById("like_tut1").onclick = function(){
+    count_tutorial1++;
+    document.getElementById("count_tut1").innerHTML = count_tutorial1;
+}
