8b2aebae4453a444c17144e0dcad67e15ce190cd
Mauricio Rivero <rivero.mauricio@gmail.com>
2024-03-23 20:54:30 -0500

    updated the html, css and js files for adding a custom and animated cursor

--- js/power-up.js
+++ js/power-up.js
@@ -5,6 +5,28 @@
 
 const colors = ["#009CDA","#FCD000","#E71E07","#42B132","#FCD000","#E71E07","#42B132","#FCD000"];
 
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function(e){
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    cursorOutline.style.left = `${posX}px`;
+    cursorOutline.style.top = `${posY}px`;
+
+    cursorOutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`
+    },{
+        duration:500,
+        fill: "forwards"
+    });
+});
+
 function textoDinamico(){
     const str = 'Retos Platzi CSS & Guías paso a paso';
     const arrayTitulo1 = Array.from(str);
